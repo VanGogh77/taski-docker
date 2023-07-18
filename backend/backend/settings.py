8f847be8e90545016ac2627ed7ca61c0a5a79787
Саша Рыбенков <rybenkov01@mail.ru>
2023-07-18 13:36:20 +0300

    add deploy 5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.73.25', '127.0.0.1', 'localhost', 'https://kittygramm.servequake.com']
+ALLOWED_HOSTS = ['158.160.73.25', '127.0.0.1', 'localhost', 'https://tasky.bounceme.net']
 
 
 # Application definition
